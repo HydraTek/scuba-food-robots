c4d544322ed97b15558da2cb144640fedd3c7a46
solack430 <94188494+solack430@users.noreply.github.com>
2021-11-19 12:04:38 -0500

    Add files via upload

---
+++ SDPrunner.m
@@ -0,0 +1,23 @@
+clc
+clear
+
+%%
+
+%start game
+game_run = input('To begin playing enter 1 into the command window: ');
+scene = simpleGameEngine();
+
+%set up background and landscape
+land=;
+
+%set up variables for sprites
+blank=;
+door=;
+
+
+while game_run==1
+    %update gameboard 
+    drawscene(land,
+end
+
+%movement

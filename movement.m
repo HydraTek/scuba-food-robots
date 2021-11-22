f79a5bba7d12166e3a3e602096e055772e539bc4
solack430 <94188494+solack430@users.noreply.github.com>
2021-11-22 15:49:55 -0500

    Create movement.m

---
+++ movement.m
@@ -0,0 +1,15 @@
+function newPosition = movement(currentPosition)
+input=getKeyboardInput();
+if isequal(input,'rightarrow')
+    newPosition=currentPosition;
+end
+if isequal(input,'leftarrow')
+    newPosition=currentPosition;
+end
+if isequal(input,'uparrow')
+    newPosition=currentPosition;
+end
+if isequal(input,'downarrow')
+    newPosition=currentPosition;
+end
+end

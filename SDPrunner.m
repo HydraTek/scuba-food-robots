630301ce3bb6d032aa1d3f74f559c5436f814647
solack430 <94188494+solack430@users.noreply.github.com>
2021-11-22 17:07:54 -0500

    Update SDPrunner.m

--- SDPrunner.m
+++ SDPrunner.m
@@ -5,19 +5,39 @@ clear
 
 %start game
 game_run = input('To begin playing enter 1 into the command window: ');
-scene = simpleGameEngine();
-
-%set up background and landscape
-land=;
+scene = simpleGameEngine('retro_pack.png',16,16,5,[121,22,22]);
 
 %set up variables for sprites
-blank=;
-door=;
+blank=1;
+character=30;
 
-
-while game_run==1
-    %update gameboard 
-    drawscene(land,
-end
+%set up the area variable and blank landscape(land) and foreground (fg) 
+area=0;
+land=zeros(16);
+land(:)=blank;
+fg=zeros(16);
+fg(:)=blank;
+fg(128)=character;
 
 %movement
+while game_run==1
+    drawScene(scene, land, fg);
+    input=getKeyboardInput(scene);
+    currentPosition=find(fg==character);
+    if isequal(input,'rightarrow')
+        newPosition=currentPosition+16;
+    end
+    if isequal(input,'leftarrow')
+        newPosition=currentPosition-16;
+    end
+    if isequal(input,'uparrow')
+        newPosition=currentPosition-1;
+    end
+    if isequal(input,'downarrow')
+        newPosition=currentPosition+1;
+    end
+    fg(currentPosition)=blank;
+    fg(newPosition)=character;
+    drawScene(scene, land, fg);
+end
+

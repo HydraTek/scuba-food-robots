67fc2e68ce202d66ca0ca87a328d69dbac9ab023
solack430 <94188494+solack430@users.noreply.github.com>
2021-11-23 00:05:30 -0500

    Create generateInt.m

---
+++ generateInt.m
@@ -0,0 +1,20 @@
+function interactiveSpace = generateInt(b,area)
+   
+   %{
+   if area==1
+   
+   end
+   if area==2
+       
+   end
+   if area==3
+       
+   end
+   if area==4
+       
+   end
+   if area==5
+       
+   end
+   %}
+end

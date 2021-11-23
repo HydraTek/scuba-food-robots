2ef88c3e622a071e7d9321e24ba83194655e751e
solack430 <94188494+solack430@users.noreply.github.com>
2021-11-22 20:23:56 -0500

    Update SDPrunner.m

--- SDPrunner.m
+++ SDPrunner.m
@@ -10,6 +10,8 @@ scene = simpleGameEngine('retro_pack.png',16,16,5,[121,22,22]);
 %set up variables for sprites
 blank=1;
 character=30;
+house=[491:493;523:525];
+enemy=28;
 
 %set up the area variable and blank landscape(land) and foreground (fg) 
 area=0;
@@ -19,25 +21,38 @@ fg=zeros(16);
 fg(:)=blank;
 fg(128)=character;
 
-%movement
-while game_run==1
-    drawScene(scene, land, fg);
-    input=getKeyboardInput(scene);
-    currentPosition=find(fg==character);
-    if isequal(input,'rightarrow')
-        newPosition=currentPosition+16;
+%initialize first sceen
+area=1;
+%fg=generateArea(fg,enemy);
+drawScene(scene, generateArea(land,fg,enemy));
+%runGame(scene,game_run,fg,character,blank,enemy);
+
+%run game
+function runGame(sc,gr,f,char,bnk,e)
+    while gr==1
+        %allow player to move via the arrow keys and then update scene to new
+        %character position
+        input=getKeyboardInput(sc);
+        currentPosition=find(f==char);
+        if isequal(input,'rightarrow')
+            newPosition=currentPosition+16;
+        end
+        if isequal(input,'leftarrow')
+            newPosition=currentPosition-16;
+        end
+        if isequal(input,'uparrow')
+            newPosition=currentPosition-1;
+        end
+        if isequal(input,'downarrow')
+            newPosition=currentPosition+1;
+        end
+        f(currentPosition)=bnk;
+        f(newPosition)=char;
+        drawScene(sc, generateArea(f,e));
     end
-    if isequal(input,'leftarrow')
-        newPosition=currentPosition-16;
-    end
-    if isequal(input,'uparrow')
-        newPosition=currentPosition-1;
-    end
-    if isequal(input,'downarrow')
-        newPosition=currentPosition+1;
-    end
-    fg(currentPosition)=blank;
-    fg(newPosition)=character;
-    drawScene(scene, land, fg);
 end
+
+  
+
+
 
